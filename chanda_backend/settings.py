7b313b7ca7f3046e423285ed6ab5c7a2c5f427e5
sivap1015844 <sivap1015844@gmail.com>
2026-09-15 23:17:36 +0530

    shut2

--- chanda_backend/settings.py
+++ chanda_backend/settings.py
@@ -98,6 +98,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+WHITENOISE_MANIFEST_STRICT = False
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
